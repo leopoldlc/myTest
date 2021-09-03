349eaf3b48c7ce5627f0ce291770a7b767c701f0
Leopoldo Lara <leopoldo.lara@laso-consulting.com>
2021-09-03 13:18:50 -0500

    Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   README.md 	new file:   add/add.py

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testing git cli
+
+This is not a real development
+This is only to learn git cli
+not real files
---
+++ add/add.py
@@ -0,0 +1,10 @@
+# This is a file for git testing 
+# this is not a real developemt
+# this was intended to learn git cli
+
+num1 =1.52
+num2 = 4.34
+
+sum= num1 + num2
+
+print("The sum of {0} and {1} is {2}".format(num1,num2,sum))
